ba1c2a40413c6c345116d6c1577f0f412cc9eca2
nasyiqin <55252513+nasyiqin@users.noreply.github.com>
2020-03-09 02:03:12 +0800

    Add files via upload

---
+++ Ques2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ques2 {
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in); //scanner input
+
+        System.out.print("Enter string: "); //user enter a string input
+
+        String str = input.next(); //read input
+
+        int count = str.length() - str.replaceAll("a","").length();
+
+        System.out.println("Amount 'a': "+ count); //display amount of a
+
+    }
+}
